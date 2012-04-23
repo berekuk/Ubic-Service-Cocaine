4856658b7a9b1798cdcde177ead932a5627de142
Vyacheslav Matyukhin <mmcleric@yandex-team.ru>
2012-04-23 23:19:05 +0400

    initial commit

---
+++ lib/Ubic/Service/Cocaine.pm
@@ -0,0 +1,96 @@
+package Ubic::Service::Cocaine;
+
+# ABSTRACT: run cocaine as Ubic service
+
+=head1 SYNOPSIS
+
+    # /etc/ubic/service/cocaine.ini:
+        module = Ubic::Service::Cocaine
+        [options]
+        endpoint = tcp://*:5000
+        storage_driver = files
+        storage_uri = /var/lib/cocaine
+
+    # alternatively, here's the perl code which you can put into /etc/ubic/service/cocaine:
+    use Ubic::Service::Cocaine;
+    Ubic::Service::Cocaine->new({
+        endpoint => 'tcp://*:5000',
+        storage_driver => 'files',
+        storage_uri => '/var/lib/cocaine',
+    });
+
+=head1 DESCRIPTION
+
+This module provides the syntax sugar for running L<https://github.com/cocaine/cocaine-core> as ubic service.
+
+Daemon's user is always B<cocaine>.
+
+=cut
+
+use strict;
+use warnings;
+
+use parent qw(Ubic::Service::SimpleDaemon);
+
+use Params::Validate qw(:all);
+
+my %opt2arg = ();
+for my $arg (qw(
+    storage:driver
+    storage:uri
+    core:modules
+    core:announce-endpoint
+    core:announce-interval
+)) {
+    my $opt = $arg;
+    $opt =~ s/:/_/g;
+    $opt =~ s/-/_/g;
+    $opt2arg{$opt} = $arg;
+}
+
+=head1 CONSTRUCTOR
+
+There is one mandatory parameter: I<endpoint>.
+
+Optional parameters are: I<storage_driver>, I<storage_uri>, I<core_modules>, I<core_announce_endpoint> and I<core_announce_interval>. There's also a boolean I<verbose> option.
+
+=cut
+sub new {
+    my $class = shift;
+
+    my $params = validate(@_, {
+        endpoint        => { type => SCALAR },
+        (
+            map {
+                $_ => { type => SCALAR, optional => 1 },
+            } keys %opt2arg,
+        ),
+        verbose => { type => BOOLEAN, optional => 1 },
+    });
+
+    my $bin = [
+        'cocained',
+        $params->{endpoint},
+        ($params->{verbose} ? '--verbose' : ()),
+        map {
+            (defined($params->{$_}) ? ("--$opt2arg{$_}" => $params->{$_}) : ()),
+        } keys %opt2arg,
+    ];
+
+    return $class->SUPER::new({
+        bin => $bin,
+        user => 'cocaine',
+    });
+}
+
+=head1 TODO
+
+As soon as Ubic will begin supporting yaml/json configs, we should replace C<core_blah> options with two-level C<< { core => { blah => ... } } >> options.
+
+TCP check would be useful.
+
+User and other SimpleDaemon parameters should be customizable.
+
+=cut
+
+1;
---
+++ t/test.t
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib 'lib';
+use Test::More;
+
+use Ubic::Service::Cocaine;
+
+pass('Look ma, no tests!');
+
+done_testing;
+
